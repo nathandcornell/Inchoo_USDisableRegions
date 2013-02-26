7b71938c6966b49102a70077cb719ad126110be0
Tomas Novoselić <tomas@inchoo.net>
2013-02-27 00:05:15 +0100

    Fixed something

--- app/code/community/USDisableRegions/Model/Resource/Region/Collection.php
+++ app/code/community/USDisableRegions/Model/Resource/Region/Collection.php
@@ -46,7 +46,7 @@ class Inchoo_USDisableRegions_Model_Resource_Region_Collection extends Mage_Dire
 
         if(!Mage::app()->getStore()->isAdmin() && Mage::getDesign()->getArea() != 'adminhtml') {
 
-            if($countryId = "US" || is_array($countryId) && implode($countryId) == "US") {
+            if($countryId == "US" || is_array($countryId) && implode($countryId) == "US") {
                 $this->addRegionNameFilter(explode(",", $allowedRegions));
             }
         }
