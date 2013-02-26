3fbfff7c879f1353c09ee5366754d7ac558b86af
Tomas Novoselić <tomas@inchoo.net>
2013-02-26 23:53:27 +0100

    Initial commit of extension files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 Inchoo_USDisableRegions
 =======================
+This extension allows you to select which regions are available to be used for
+customer address creation.
+
+It works only for US.
+
+Extension is tested only on Magento ver. 1.7.0.2
+
+Keep in mind that this extension is created for purposes of blog article at inchoo.net and as such can
+only be used as proof of concept. If you find it helpful in order to learn Magento, that is great, but be careful
+if you plan to use it in production environment.
---
+++ app/code/community/USDisableRegions/Model/Resource/Region/Collection.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Inchoo
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Please do not edit or add to this file if you wish to upgrade
+ * Magento or this extension to newer versions in the future.
+ * Inchoo developers (Inchooer's) give their best to conform to
+ * "non-obtrusive, best Magento practices" style of coding.
+ * However, Inchoo does not guarantee functional accuracy of
+ * specific extension behavior. Additionally we take no responsibility
+ * for any possible issue(s) resulting from extension usage.
+ * We reserve the full right not to provide any kind of support for our free extensions.
+ * Thank you for your understanding.
+ *
+ * @category    Inchoo
+ * @package     Inchoo_USDisableRegions
+ * @author      Tomas Novoselić <tomas@inchoo.net>
+ * @copyright   Copyright (c) Inchoo (http://inchoo.net/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Inchoo_USDisableRegions_Model_Resource_Region_Collection extends Mage_Directory_Model_Resource_Region_Collection
+{
+    public function addCountryFilter($countryId)
+    {
+        if (!empty($countryId)) {
+            if (is_array($countryId)) {
+                $this->addFieldToFilter('main_table.country_id', array('in' => $countryId));
+            } else {
+                $this->addFieldToFilter('main_table.country_id', $countryId);
+            }
+        }
+
+        $allowedRegions = Mage::getStoreConfig('general/enabled_regions/region');
+
+        if(!Mage::app()->getStore()->isAdmin() && Mage::getDesign()->getArea() != 'adminhtml') {
+
+            if($countryId = "US" || is_array($countryId) && implode($countryId) == "US") {
+                $this->addRegionNameFilter(explode(",", $allowedRegions));
+            }
+        }
+
+        return $this;
+    }
+}
